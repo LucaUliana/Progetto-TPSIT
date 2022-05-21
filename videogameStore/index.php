97d579991ed982a411c961bbaa45ecb523bca1a4
mattiaCarniel <103113219+mattiaCarniel@users.noreply.github.com>
2022-05-21 10:33:34 +0200

    Update index.php

--- videogameStore/index.php
+++ videogameStore/index.php
@@ -1,23 +1,23 @@
 <html>
     <head>
-		<title>Videogame Shop</title>
+	<title>Videogame Shop</title>
 	<link rel="stylesheet" href="style.css">
 	</head>
     <body>
-		<header>
-			<nav>
-				<ul>
-					<li class="nav-logo">Videogame Store</li>
-				</ul>
-			</nav>
-			<button class="nav">Profile</button>
-		</header>
+	<header>
+		<nav>
+			<ul>
+				<li class="nav-logo">Videogame Store</li>
+			</ul>
+		</nav>
+		<button class="nav">Profile</button>
+	</header>
 		
-		<div class="container">
-			<button class="menu">Store</button>
-			<button class="menu">Library</button>
-			<button class="menu">Settings</button>
-		</div>
+	<div class="container">
+		<button class="menu">Store</button>
+		<button class="menu">Library</button>
+		<button class="menu">Settings</button>
+	</div>
 
     </body>
 </html>
