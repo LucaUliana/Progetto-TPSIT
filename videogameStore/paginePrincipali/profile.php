be57a25e6c91f887124dd2154498dd5a5c1ac2d1
LucaUliana <103306213+LucaUliana@users.noreply.github.com>
2022-06-01 17:13:35 +0200

    modifica profile.php

--- videogameStore/paginePrincipali/profile.php
+++ videogameStore/paginePrincipali/profile.php
@@ -4,5 +4,25 @@
         <link rel="stylesheet" href="../css/style.css">
     </head>
     <body>
+        <header>
+			<nav>
+				<ul>
+					<li class="nav-logo">VIDEOGAME STORE</li>
+				</ul>
+			</nav>
+			<a href="home.php">
+			<button class="browsing">Home</button>
+			</a>
+			
+			<a href="store.php">
+			<button class="browsing">Store</button>
+			</a>
+		</header>
+		<form action="profile" method="post">
+			<p>Nome</p> <input type="text" minlength="0" name="firstName" placeholder value>
+			<p>Cognome</p> <input type="text" minlength="0" name="SecondName" placeholder value>
+			<p>Nome Utente</p> <input type="text" minlength="0" name="Name" placeholder value><br>
+			<button type="submit" value="submit">SALVA I DATI</button>
+		</form>	
     </body>
 </html>
