4aa3fa636e20f84e23950db30ccbba5226032951
mattiaCarniel <103113219+mattiaCarniel@users.noreply.github.com>
2022-06-03 17:09:47 +0200

    aggiunta descrizione

--- videogameStore/classi/Game.php
+++ videogameStore/classi/Game.php
@@ -7,14 +7,16 @@ class Game{
     public $prezzo;
     public $immagine;
     public $acquistato;
+    public $descrizione;
 
-    function __construct($pNome, $pNomeSviluppatore, $pValutazione, $pPrezzo, $pimmagine, $pAcquistato){
+    function __construct($pNome, $pNomeSviluppatore, $pValutazione, $pPrezzo, $pimmagine, $pAcquistato, $pDescrizione){
         $this->nome = $pNome;
         $this->nomeSviluppatore = $pNomeSviluppatore;
         $this->valutazione = $pValutazione;
         $this->prezzo = $pPrezzo;
         $this->immagine = $pimmagine;
         $this->acquistato = $pAcquistato;
+        $this->descrizione = $pDescrizione;
     }
 }
     
