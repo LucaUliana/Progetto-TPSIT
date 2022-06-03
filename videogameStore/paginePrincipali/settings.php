bd717f5f5d0fff4ef5aefda0055b094cfbd8449e
AlexBoskovic <103113280+AlexBoskovic@users.noreply.github.com>
2022-06-03 18:29:59 +0200

    Fixate le impostazioni

--- videogameStore/paginePrincipali/settings.php
+++ videogameStore/paginePrincipali/settings.php
@@ -10,9 +10,15 @@
 					<li class="nav-logo">VIDEOGAME STORE</li>
 				</ul>
 			</nav>
+			<a href="../home.php">
+			<button class="browsing">Home</button>
+			</a>
 			<a href="profile.php">
 			<button class="nav">Profile</button>
 			</a>
 		</header>
+			<button class="browsing" id="scuro">Tema Scuro</button>
+
+			<button class="browsing" id="chiaro">Tema Chiaro</button>
     </body>
 </html>
