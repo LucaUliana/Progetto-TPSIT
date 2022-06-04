bd2dc9711bddb274fa83321cc78d718f1c36fc99
AlexBoskovic <103113280+AlexBoskovic@users.noreply.github.com>
2022-06-04 09:20:32 +0200

    Aggiunto image link

--- videogameStore/paginePrincipali/store.php
+++ videogameStore/paginePrincipali/store.php
@@ -40,7 +40,7 @@
 			echo "
 			<form method='get' action='gamePage.php'>
 				<div class=\"gioco\">
-				<img class='immagineGioco' src='$obj->immagine'/>
+				<a href='gamePage.php?percorso=$obj->nome'> <img class='immagineGioco' src='$obj->immagine'/> </a>
 					<button class='menu' type='submit' name='percorso' value='$obj->nome'> $obj->nome </button>
 				</div>
 			</form>";
